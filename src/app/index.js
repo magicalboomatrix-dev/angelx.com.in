a538389843c53a2dfbe2d7b26491e98cc67af841
magicalboomatrix-dev <magicalboomatrix@gmail.com>
2026-03-16 17:25:14 +0530

    Update index.js

--- src/app/index.js
+++ src/app/index.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Link from 'next/link';
 
 import FAQ from "./components/faq";
-import TetimonialCarousel from "./components/TetimonialCarousel";
+import TetimonialCarousel from "./components/tetimonials";
 import Footer from './components/footer';
 import Readmore from "./components/Readmore";
 
