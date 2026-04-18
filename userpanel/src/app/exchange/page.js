57b33f00f21c3ea46cc1aee69c32aa40d2ae9c20
magicalboomatrix-dev <magicalboomatrix@gmail.com>
2026-04-18 16:22:16 +0530

    Update page.js

--- userpanel/src/app/exchange/page.js
+++ userpanel/src/app/exchange/page.js
@@ -1,5 +1,6 @@
 "use client";
 import React, { useState, useEffect, useCallback } from "react";
+import Image from "next/image";
 import dynamic from "next/dynamic";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
